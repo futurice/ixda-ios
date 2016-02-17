e263a9acc613df161ab4d8ddc37be2a79a313b5a
Evangelos Sismanidis <evangelos.sismanidis@futurice.com>
2016-02-17 20:56:38 +0200

    added info view controller

---
+++ interaction16/Controllers/InfoAndVenue/IXDAInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  IXDAInfoViewController.h
+//  interaction16
+//
+//  Created by Evangelos Sismanidis on 17/02/16.
+//  Copyright © 2016 Futurice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IXDAInfoViewController : UIViewController
+
+@end
