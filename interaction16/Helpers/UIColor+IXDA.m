d16b9259e926de6babfe96728d225ea723b8d570
Evangelos Sismanidis <evangelos.sismanidis@futurice.com>
2016-02-17 20:40:49 +0200

    minor adjustments to speakers

--- interaction16/Helpers/UIColor+IXDA.m
+++ interaction16/Helpers/UIColor+IXDA.m
@@ -30,4 +30,8 @@
     return [UIColor colorWithRed:0 green:0.27 blue:0.93 alpha:1];
 }
 
++ (UIColor *)ixda_speakersTableViewCellBlurColor {
+    return [UIColor colorWithRed:0 green:0.408 blue:0.941 alpha:0.4];
+}
+
 @end
